ed9826ebf2a555b2d96437d43adade50efd34011
Darshit <darshitparekh30@gmail.com>
2020-06-05 10:24:56 +0530

    Basic setup

---
+++ src/test/java/NumberTest.java
@@ -0,0 +1,26 @@
+import com.sapient.Number;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class NumberTest {
+
+	private Number ob;
+	@BeforeEach
+	public void setNumberObject() {
+		
+		ob = new Number();
+	}
+	
+	@Test
+	public void checkPrimeReturnTrue() {
+		assertTrue(1,ob.checkPrime(10));
+	}
+	
+	@AfterEach
+	public void destroyNumberObject() {
+		ob = null;
+	}
+}
